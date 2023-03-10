5811b8731dff6ca906235e24f81a0498841ae00b
Dmytro Trotsenko <trohae7@gmail.com>
2023-03-10 11:12:18 +0200

    Create project

---
+++ src/main/java/com/estore/EStoreApp.java
@@ -0,0 +1,13 @@
+package com.estore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EStoreApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EStoreApp.class, args);
+    }
+
+}
