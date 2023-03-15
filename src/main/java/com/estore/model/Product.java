16ae0dff9b396d2e2087905a751207bc298bacf3
Dmytro Trotsenko <trohae7@gmail.com>
2023-03-15 19:20:17 +0200

    Fixed for work with R2DBC

---
+++ src/main/java/com/estore/model/Product.java
@@ -0,0 +1,34 @@
+package com.estore.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.math.BigDecimal;
+
+/**
+ * {@link Product}
+ *
+ * @author Dmytro Trotsenko on 3/9/23
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("e_store.product")
+public class Product {
+
+    @Id
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private BigDecimal price;
+
+}
