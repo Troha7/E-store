be690d4a18558c86a81d98dbc7070733c4f32f7f
Dmytro Trotsenko <trohae7@gmail.com>
2023-03-15 19:16:34 +0200

    Add @EnableWebFlux for configuration R2DBC

--- src/main/java/com/estore/EStoreApp.java
+++ src/main/java/com/estore/EStoreApp.java
@@ -2,8 +2,10 @@ package com.estore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class EStoreApp {
 
     public static void main(String[] args) {
