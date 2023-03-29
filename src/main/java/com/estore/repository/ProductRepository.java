11fd2a22392f79ae7dfaa5a28878afeaeea4411c
Dmytro Trotsenko <trohae7@gmail.com>
2023-03-29 11:13:25 +0300

    Added custom query - findProductsByOrderId

--- src/main/java/com/estore/repository/ProductRepository.java
+++ src/main/java/com/estore/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.estore.repository;
 
 import com.estore.model.Product;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -18,5 +19,13 @@ public interface ProductRepository extends ReactiveCrudRepository<Product, Long>
     Mono<Product> findByName(String name);
 
     Flux<Product> findByNameContaining(String name);
+
+    @Query("select p.*, oi.quantity " +
+            "from e_store.product p " +
+            "join e_store.order_item oi " +
+            "on p.id = oi.fk_product_id " +
+            "where oi.fk_order_id = :item_id " +
+            "order by p.name")
+    Flux<Product> findProductsByOrderId(Long orderId);
 
 }
