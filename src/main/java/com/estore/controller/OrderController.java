e3e639515d4c287f3c2f12b18d4ac708d21d160a
Dmytro Trotsenko <trohae7@gmail.com>
2023-03-30 18:10:47 +0300

    Added Swagger-ui web-API description

--- src/main/java/com/estore/controller/OrderController.java
+++ src/main/java/com/estore/controller/OrderController.java
@@ -5,6 +5,8 @@ import com.estore.dto.request.OrderRequestDto;
 import com.estore.dto.response.OrderItemResponseDto;
 import com.estore.dto.response.OrderResponseDto;
 import com.estore.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -22,42 +24,49 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/orders")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "Orders")
 public class OrderController {
 
     private final OrderService orderService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a new Order")
     public Mono<OrderResponseDto> createOrder() {
         return orderService.create();
     }
 
     @PostMapping("/{orderId}")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Add product and quantity an Order by order id")
     public Mono<OrderItemResponseDto> addProduct(@PathVariable long orderId, @RequestBody OrderItemRequestDto orderItem) {
         return orderService.addProduct(orderId, orderItem);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Update an existing Order")
     public Mono<OrderResponseDto> update(@PathVariable long id, @RequestBody OrderRequestDto order) {
         return orderService.update(id, order);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Find all Orders")
     public Flux<OrderResponseDto> findAll() {
         return orderService.findAll();
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Find an Order by id")
     public Mono<OrderResponseDto> findById(@PathVariable("id") long id) {
         return orderService.findById(id);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete an Order by id")
     public Mono<Void> delete(@PathVariable("id") long id) {
         return orderService.deleteById(id);
     }
--- src/main/java/com/estore/controller/ProductController.java
+++ src/main/java/com/estore/controller/ProductController.java
@@ -3,6 +3,9 @@ package com.estore.controller;
 import com.estore.dto.response.ProductResponseDto;
 import com.estore.dto.request.ProductRequestDto;
 import com.estore.service.ProductService;
+//import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -20,42 +23,49 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/products")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "Products")
 public class ProductController {
 
     private final ProductService productService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Find all Products (if not set RequestParam) or Find by containing name")
     public Flux<ProductResponseDto> getProducts(@RequestParam(required = false) String name) {
         return (name == null) ? productService.findAll() : productService.findByNameContaining(name);
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Find an Product by id")
     public Mono<ProductResponseDto> getProductById(@PathVariable("id") long id) {
         return productService.findById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a new Product")
     public Mono<ProductResponseDto> createProduct(@RequestBody ProductRequestDto product) {
         return productService.create(product);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Update an existing Product")
     public Mono<ProductResponseDto> updateProduct(@PathVariable long id, @RequestBody ProductRequestDto product) {
         return productService.update(id, product);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete an Product by id")
     public Mono<Void> deleteProduct(@PathVariable("id") long id) {
         return productService.deleteById(id);
     }
 
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete all Products")
     public Mono<Void> deleteAllProducts() {
         return productService.deleteAll();
     }
