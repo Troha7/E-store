2ef12398a554453a45e0210d2c9d5ae6d3eb51f5
Dmytro Trotsenko <trohae7@gmail.com>
2023-04-08 23:51:00 +0300

    Deleted product list

--- src/main/java/com/estore/model/Order.java
+++ src/main/java/com/estore/model/Order.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDate;
-import java.util.List;
 
 /**
  * {@link Order}
@@ -31,8 +29,5 @@ public class Order {
 
     @Column("order_date")
     private LocalDate date;
-
-    @Transient
-    private List<Product> Products;
 
 }
