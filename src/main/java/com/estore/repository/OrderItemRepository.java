096b97b3d981be560ae3f8281aeda850a125a827
Dmytro Trotsenko <trohae7@gmail.com>
2023-04-08 23:36:24 +0300

    Added custom query findProductsByOrderId and existByOrderIdAndProductId

--- src/main/java/com/estore/repository/OrderItemRepository.java
+++ src/main/java/com/estore/repository/OrderItemRepository.java
@@ -1,6 +1,8 @@
 package com.estore.repository;
 
 import com.estore.model.OrderItem;
+import com.estore.model.Product;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -17,6 +19,18 @@ import reactor.core.publisher.Mono;
 public interface OrderItemRepository extends ReactiveCrudRepository<OrderItem, Long> {
 
     Flux<OrderItem> findAllByOrderId(Long id);
+
     Mono<Void> deleteAllByOrderId(Long id);
+
+    @Query("SELECT * " +
+            "FROM e_store.product p " +
+            "JOIN e_store.order_item oi ON p.id = oi.fk_product_id " +
+            "WHERE oi.fk_order_id = :order_id;")
+    Flux<Product> findProductsByOrderId(Long orderId);
+
+    @Query("SELECT o.id > 0 AND p.id > 0 " +
+            "FROM e_store.product p, e_store.order o " +
+            "WHERE  o.id=:order_id AND p.id=:prod_id;")
+    Mono<Boolean> existByOrderIdAndProductId(Long orderId, Long productId);
 
 }
