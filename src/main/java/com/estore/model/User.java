b6ecb65ed5f4b686761d296c4b4191217fc09d5b
Dmytro Trotsenko <trohae7@gmail.com>
2023-04-20 23:20:53 +0300

    Create User entity

---
+++ src/main/java/com/estore/model/User.java
@@ -0,0 +1,40 @@
+package com.estore.model;
+
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+
+/**
+ * {@link User}
+ *
+ * @author Dmytro Trotsenko on 3/9/23
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("e_store.user")
+public class User {
+
+    @Id
+    private Long id;
+
+    @Column("fk_address_id")
+    private Long addressId;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String password;
+
+}
