9871998eac8f7d2b382003a4d773f73f30de6a5a
Dmytro Trotsenko <trohae7@gmail.com>
2023-04-22 21:26:12 +0300

    Added POST methods tests

--- src/test/java/com/estore/controller/OrderControllerTest.java
+++ src/test/java/com/estore/controller/OrderControllerTest.java
@@ -1,13 +1,13 @@
 package com.estore.controller;
 
 import com.estore.configuration.TestContainerConfig;
-import com.estore.dto.request.ProductRequestDto;
+import com.estore.dto.request.OrderItemRequestDto;
 import com.estore.dto.response.OrderItemWithProductResponseDto;
 import com.estore.dto.response.OrderWithProductsResponseDto;
 import com.estore.model.Product;
 import com.estore.repository.ProductRepository;
 import com.estore.service.OrderService;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,8 +18,10 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
@@ -43,8 +45,6 @@ public class OrderControllerTest {
     private ProductRepository productRepository;
     @Autowired
     private OrderService orderService;
-    @Autowired
-    private ObjectMapper objectMapper;
 
     private WebTestClient webTestClient;
 
@@ -53,12 +53,19 @@ public class OrderControllerTest {
 
     private static final String URI = "/orders";
 
-    private final Long NOT_EXISTED_ID = 100L;
+    private final Long NOT_EXISTED_ORDER_ID = 100L;
+    private final Long NOT_EXISTED_PRODUCT_ID = 100L;
 
-    private final List<ProductRequestDto> products = List.of(
-            new ProductRequestDto("laptop", "Lenovo", BigDecimal.valueOf(3550.95)),
-            new ProductRequestDto("phone", "Xiaomi", BigDecimal.valueOf(6700.55)),
-            new ProductRequestDto("smartTV", "Samsung", BigDecimal.valueOf(9670.19))
+    private final List<Product> products = List.of(
+            new Product(1L, "laptop", "Lenovo", BigDecimal.valueOf(3550.95)),
+            new Product(2L, "phone", "Xiaomi", BigDecimal.valueOf(6700.55)),
+            new Product(3L, "smartTV", "Samsung", BigDecimal.valueOf(9670.19))
+    );
+
+    private final List<OrderItemRequestDto> orderItems = List.of(
+            new OrderItemRequestDto(products.get(0).getId(), 1),
+            new OrderItemRequestDto(products.get(1).getId(), 2),
+            new OrderItemRequestDto(products.get(2).getId(), 3)
     );
 
     @BeforeEach
@@ -94,7 +101,7 @@ public class OrderControllerTest {
     void shouldReturnAllOrdersWithProducts() {
 
         int ordersNum = 3;
-        var savedOrders = createOrders(ordersNum);
+        var emptyOrders = createOrdersWithProducts(ordersNum);
 
         webTestClient.get().uri(URI)
                 .exchange()
@@ -102,15 +109,103 @@ public class OrderControllerTest {
                 .expectBodyList(OrderWithProductsResponseDto.class)
                 .value(orderList -> {
                     assertEquals(ordersNum, orderList.size());
-                    assertOrderListEquals(savedOrders, orderList);
+                    assertOrderListEquals(emptyOrders, orderList);
                 });
     }
+
+    @Test
+    void shouldReturnOrderById() {
+
+        var savedOrderWithProducts = createOrdersWithProducts(3).get(0);
+        Long id = savedOrderWithProducts.getId();
+
+        webTestClient.get().uri(URI.concat("/{id}"), id)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(OrderWithProductsResponseDto.class)
+                .value(order -> assertOrderEquals(savedOrderWithProducts, order));
+    }
+
+    @Test
+    void shouldThrowExceptionIfOrderIdDoesNotExist() {
+
+        webTestClient.get().uri(URI.concat("/{id}"), NOT_EXISTED_ORDER_ID)
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
+    //-----------------------------------
+    //               POST
+    //-----------------------------------
+
+    @Test
+    void shouldCreatedNewOrder() {
+
+        var expectedOrder = new OrderWithProductsResponseDto(null, LocalDate.now(), null);
+
+        webTestClient.post().uri(URI)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(OrderWithProductsResponseDto.class)
+                .value(order -> assertOrderEquals(expectedOrder, order));
+
+        orderService.findAll()
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldAddedProductToOrderById() {
+
+        var savedOrder = orderService.create().block();
+        assertNotNull(savedOrder);
+        Long id = savedOrder.getId();
+
+        List<OrderItemWithProductResponseDto> addedOrderItemList = List.of(
+                new OrderItemWithProductResponseDto(null, products.get(0), 1));
+        var addedOrderWithProducts = new OrderWithProductsResponseDto(null, LocalDate.now(), addedOrderItemList);
+
+        webTestClient.post().uri(URI.concat("/{id}"), id)
+                .bodyValue(orderItems.get(0))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(OrderWithProductsResponseDto.class)
+                .value(order -> assertOrderEquals(addedOrderWithProducts, order));
+    }
+
+    @Test
+    void shouldThrowExceptionIfAddedProductIdDoesNotExist() {
+
+        var savedOrder = orderService.create().block();
+        assertNotNull(savedOrder);
+        Long id = savedOrder.getId();
+
+        var orderItem = new OrderItemRequestDto(NOT_EXISTED_PRODUCT_ID, 100);
+
+        webTestClient.post().uri(URI.concat("/{id}"), id)
+                .bodyValue(orderItem)
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
+    @Test
+    void shouldThrowExceptionAddedProductIfOrderIdDoesNotExist() {
+
+        orderService.create().block();
+
+        webTestClient.post().uri(URI.concat("/{id}"), NOT_EXISTED_ORDER_ID)
+                .bodyValue(orderItems.get(1))
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
 
     //-----------------------------------
     //         Private methods
     //-----------------------------------
 
-    private void saveProductsIfNotExist(List<ProductRequestDto> productList) {
+    private void saveProductsIfNotExist(List<Product> productList) {
         Flux.fromIterable(productList)
                 .map(p -> new Product(null, p.getName(), p.getDescription(), p.getPrice()))
                 .flatMap(p -> productRepository.findByName(p.getName())
@@ -118,30 +213,39 @@ public class OrderControllerTest {
                 .subscribe();
     }
 
-    private List<OrderWithProductsResponseDto> createOrders(int num) {
+    private @NotNull List<OrderWithProductsResponseDto> createOrdersWithProducts(int num) {
         List<OrderWithProductsResponseDto> orders = new ArrayList<>();
         for (int i = 0; i < num; i++) {
-            orders.add(orderService.create().block());
+            var savedOrderWithProducts = orderService.create()
+                    .flatMap(o -> orderService.addProductByOrderId(o.getId(), orderItems.get(0))
+                            .then(orderService.addProductByOrderId(o.getId(), orderItems.get(1)))
+                            .then(orderService.addProductByOrderId(o.getId(), orderItems.get(2))))
+                    .block();
+            orders.add(savedOrderWithProducts);
         }
         return orders;
     }
 
-    private void assertOrderListEquals(List<OrderWithProductsResponseDto> expectedOrders, List<OrderWithProductsResponseDto> actualOrders){
+    private void assertOrderListEquals(List<OrderWithProductsResponseDto> expectedOrders, @NotNull List<OrderWithProductsResponseDto> actualOrders) {
         IntStream.range(0, actualOrders.size())
                 .forEach(i -> assertOrderEquals(expectedOrders.get(i), actualOrders.get(i)));
     }
 
-    private void assertOrderEquals(OrderWithProductsResponseDto expectedOrder, OrderWithProductsResponseDto actualOrder) {
+    private void assertOrderEquals(@NotNull OrderWithProductsResponseDto expectedOrder, @NotNull OrderWithProductsResponseDto actualOrder) {
         assertEquals(expectedOrder.getDate(), actualOrder.getDate());
         assertOrderItemListEquals(expectedOrder.getOrderItems(), actualOrder.getOrderItems());
     }
 
     private void assertOrderItemListEquals(List<OrderItemWithProductResponseDto> expectedOrderItems, List<OrderItemWithProductResponseDto> actualOrderItems) {
-        IntStream.range(0, actualOrderItems.size())
-                .forEach(i -> assertOrderItemEquals(expectedOrderItems.get(i), actualOrderItems.get(i)));
+        if (actualOrderItems == null) {
+            assertNull(expectedOrderItems);
+        } else {
+            IntStream.range(0, actualOrderItems.size())
+                    .forEach(i -> assertOrderItemEquals(expectedOrderItems.get(i), actualOrderItems.get(i)));
+        }
     }
 
-    private void assertOrderItemEquals(OrderItemWithProductResponseDto expectedOrderItem, OrderItemWithProductResponseDto actualOrderItem) {
+    private void assertOrderItemEquals(@NotNull OrderItemWithProductResponseDto expectedOrderItem, @NotNull OrderItemWithProductResponseDto actualOrderItem) {
         assertEquals(expectedOrderItem.getProduct(), actualOrderItem.getProduct());
         assertEquals(expectedOrderItem.getQuantity(), actualOrderItem.getQuantity());
     }
