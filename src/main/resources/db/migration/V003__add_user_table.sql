8299d3a8f0c0cca73c2b2203cc59ff05260c9a99
Dmytro Trotsenko <trohae7@gmail.com>
2023-05-11 23:17:53 +0300

    Fixed DB tables

--- src/main/resources/db/migration/V003__add_user_table.sql
+++ src/main/resources/db/migration/V003__add_user_table.sql
@@ -2,10 +2,8 @@
 CREATE TABLE IF NOT EXISTS e_store.user
 (
     id              SERIAL CONSTRAINT user_id_pkey PRIMARY KEY,
-    fk_address_id   BIGINT CONSTRAINT address_id_fkey
-                    REFERENCES e_store.address (id) ON DELETE CASCADE,
     name            VARCHAR(25) NOT NULL UNIQUE,
     email           VARCHAR(45) NOT NULL UNIQUE,
-    phone           VARCHAR(10) NOT NULL UNIQUE,
+    phone           VARCHAR(13) NOT NULL UNIQUE,
     password        VARCHAR(64) NOT NULL
 );
--- src/main/resources/db/migration/V004__add_address_table.sql
+++ src/main/resources/db/migration/V004__add_address_table.sql
@@ -2,6 +2,8 @@
 CREATE TABLE IF NOT EXISTS e_store.address
 (
     id              SERIAL CONSTRAINT address_id_pkey PRIMARY KEY,
+    fk_user_id      BIGINT CONSTRAINT user_id_fkey
+                    REFERENCES e_store.user (id) ON DELETE CASCADE,
     city            VARCHAR(15) NOT NULL,
     street          VARCHAR(15) NOT NULL,
     house           VARCHAR(5) NOT NULL
