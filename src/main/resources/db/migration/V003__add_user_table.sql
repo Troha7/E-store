a7a57492a8e73f47e7e948246b0b643aeab4053c
Dmytro Trotsenko <trohae7@gmail.com>
2023-05-26 09:57:28 +0300

    Fixed SQL tables

--- src/main/resources/db/migration/V003__add_user_table.sql
+++ src/main/resources/db/migration/V003__add_user_table.sql
@@ -2,8 +2,12 @@
 CREATE TABLE IF NOT EXISTS e_store.user
 (
     id              SERIAL CONSTRAINT user_id_pkey PRIMARY KEY,
-    name            VARCHAR(25) NOT NULL UNIQUE,
-    email           VARCHAR(45) NOT NULL UNIQUE,
-    phone           VARCHAR(13) NOT NULL UNIQUE,
-    password        VARCHAR(64) NOT NULL
+    username        VARCHAR(64) NOT NULL UNIQUE,
+    password        VARCHAR(2048) NOT NULL,
+    role            VARCHAR(32) NOT NULL,
+    first_name      VARCHAR(64) NOT NULL,
+    last_name       VARCHAR(64) NOT NULL,
+    email           VARCHAR(64) NOT NULL UNIQUE,
+    phone           VARCHAR(13) NOT NULL UNIQUE
+
 );
