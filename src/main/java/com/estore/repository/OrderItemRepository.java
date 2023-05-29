9086a8ca8233aed332b9ca5023edfdb7a7a72d8c
Dmytro Trotsenko <trohae7@gmail.com>
2023-05-29 10:45:20 +0300

    Replace method findProductsByOrderId to ProductRepository And Fixed query in existByOrderIdAndProductId

--- src/main/java/com/estore/repository/OrderItemRepository.java
+++ src/main/java/com/estore/repository/OrderItemRepository.java
@@ -1,7 +1,6 @@
 package com.estore.repository;
 
 import com.estore.model.OrderItem;
-import com.estore.model.Product;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
@@ -20,17 +19,11 @@ public interface OrderItemRepository extends ReactiveCrudRepository<OrderItem, L
 
     Flux<OrderItem> findAllByOrderId(Long id);
 
-    Mono<Void> deleteAllByOrderId(Long id);
-
-    @Query("SELECT * " +
-            "FROM e_store.product p " +
-            "JOIN e_store.order_item oi ON p.id = oi.fk_product_id " +
-            "WHERE oi.fk_order_id = :order_id;")
-    Flux<Product> findProductsByOrderId(Long orderId);
-
-    @Query("SELECT o.id > 0 AND p.id > 0 " +
-            "FROM e_store.product p, e_store.order o " +
-            "WHERE  o.id=:order_id AND p.id=:prod_id;")
+    @Query("""
+            SELECT COUNT(*) > 0
+            FROM e_store.product p, e_store.order o
+            WHERE  o.id=:order_id AND p.id=:product_id;
+            """)
     Mono<Boolean> existByOrderIdAndProductId(Long orderId, Long productId);
 
 }
