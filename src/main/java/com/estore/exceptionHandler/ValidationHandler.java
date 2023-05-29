6ec14707ac0fec47aba5c0a09cf07963ddcfa1a6
Dmytro Trotsenko <trohae7@gmail.com>
2023-05-29 10:53:46 +0300

    Created ValidationHandler for customizing a message for an invalid field

---
+++ src/main/java/com/estore/exceptionHandler/ValidationHandler.java
@@ -0,0 +1,35 @@
+package com.estore.exceptionHandler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * {@link ValidationHandler} is the class for customizing a message for an invalid field
+ *
+ * @author Dmytro Trotsenko on 5/27/23
+ */
+
+@ControllerAdvice
+public class ValidationHandler {
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<List<String>> handleException(WebExchangeBindException e) {
+
+        //Create custom error message
+        var customErrMessage = IntStream.range(0, e.getBindingResult().getAllErrors().size())
+                .mapToObj(i -> {
+                    String message = e.getBindingResult().getAllErrors().get(i).getDefaultMessage();
+                    String field = e.getFieldErrors().get(i).getField();
+                    return String.format("Invalid '%s': %s", field, message);
+                })
+                .toList();
+
+        return ResponseEntity.badRequest().body(customErrMessage);
+    }
+
+}
