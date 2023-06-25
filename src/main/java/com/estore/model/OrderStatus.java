4b5f1afbccfa36b888904e6a1f50becd8cf7f9fb
Dmytro Trotsenko <trohae7@gmail.com>
2023-06-25 18:01:42 +0300

    Added Order status enum

---
+++ src/main/java/com/estore/model/OrderStatus.java
@@ -0,0 +1,17 @@
+package com.estore.model;
+
+/**
+ * {@link OrderStatus}
+ *
+ * @author Dmytro Trotsenko on 5/18/23
+ */
+public enum OrderStatus {
+
+    CREATED,
+    ACCEPTED,
+    PAID,
+    SHIPPING,
+    COMPLETED,
+    CANCELED
+
+}
