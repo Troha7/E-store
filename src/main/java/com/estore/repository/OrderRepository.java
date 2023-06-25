2dedee574d6b7ad576e95b096ea48e51aaf998fc
Dmytro Trotsenko <trohae7@gmail.com>
2023-06-25 18:17:48 +0300

    Added findAllOrderByUserId

--- src/main/java/com/estore/repository/OrderRepository.java
+++ src/main/java/com/estore/repository/OrderRepository.java
@@ -1,8 +1,11 @@
 package com.estore.repository;
 
 import com.estore.model.Order;
+import com.estore.model.OrderStatus;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 
 /**
@@ -14,4 +17,13 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderRepository extends ReactiveCrudRepository<Order, Long> {
 
+    Flux<Order> findAllOrderByUserId(Long id);
+
+        @Query("""
+                SELECT *
+                FROM e_store.order
+                WHERE status = :status 
+                AND fk_user_id = (SELECT id FROM e_store.user WHERE username = :username)
+                """)
+    Flux<Order> findAllOrderByUsernameAndStatus(String username, OrderStatus status);
 }
